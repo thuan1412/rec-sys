8d0fb5be63275d3f102861bc4088c286fa3a278d
huongpham <huongphama3@email.com>
2021-10-23 16:26:40 +0700

    update

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,4 +8,4 @@ app_name = 'products'
 urlpatterns = [
     path('auth', views.detail, name='index'),
     path('', views.homepage, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
